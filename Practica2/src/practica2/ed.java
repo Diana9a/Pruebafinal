3395aa83956658c7534c0bb328f531965d4d8406
Diana9a <diosvidal.diana@gmail.com>
2014-05-16 13:43:24 +0200

    Nuevo Pro

---
+++ Practica2/src/practica2/ed.java
@@ -0,0 +1,14 @@
+package practica2;
+
+public class ed {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Haciendo la práctica de entornos");
+		System.out.println("Nuevo archivo");
+	}
+
+}
